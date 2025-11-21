812440bc8fe8b15373a101f41126d804c414b448
sharavudayagiri <akshvuday3@gmail.com>
2025-11-20 22:57:50 -0600

    Had to update pom.xml to be able to run all my tests

--- ClosetOrganizer/src/test/java/org/example/models/WishlistItemTest.java
+++ ClosetOrganizer/src/test/java/org/example/models/WishlistItemTest.java
@@ -1,7 +1,7 @@
 package org.example.models;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class WishlistItemTest {
 
@@ -11,7 +11,7 @@ public class WishlistItemTest {
         WishlistItem item = new WishlistItem(1, "Beige Long Sleeve Shirt", "Target", 25.00, "https://www.target.com/p/women-s-long-sleeve-ruched-blouse-a-new-day-beige/-/A-94960322?preselect=94960370#lnk=sametab");
 
         assertEquals(1, item.getId());
-        assertEquals("", item.getItemName());
+        assertEquals("Beige Long Sleeve Shirt", item.getItemName());
         assertEquals("Target", item.getStoreName());
         assertEquals(25.00, item.getPrice());
         assertEquals("https://www.target.com/p/women-s-long-sleeve-ruched-blouse-a-new-day-beige/-/A-94960322?preselect=94960370#lnk=sametab", item.getLink());
